0f52bf45c89028e44ddc1f7cb2e2b0e929692bbb
MarcoCatarino <marco.catarino60@gmail.com>
2025-10-04 14:50:28 -0500

    Configuracion Hyprland v2

--- hypr/scripts/launch-waybar.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Lanzar waybar en background con nohup directamente
-nohup waybar -c ~/.config/waybar/config-laptop.json -s ~/.config/waybar/style-laptop.css >/dev/null 2>&1 &
-nohup waybar -c ~/.config/waybar/config-external.json -s ~/.config/waybar/style-external.css >/dev/null 2>&1 &
